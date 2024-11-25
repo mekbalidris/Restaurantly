21f36aeff295c49f7cfa5fd811a4506932733801
mekbalidris <mekbalidriss@gmail.com>
2024-11-25 23:45:50 +0100

    updated the about page

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,22 +7,25 @@ export default {
   theme: {
     extend: {
       colors: {
-
+        'mainbg': '#6c757d', 
       },
       screens:{
         lg: '1100px',
+        mid: '992px',
       },
       width:{
         '100': '100%',
         '80' : '80%',
         '70' : '500px',
+        '17' : '21rem'
       },
       height:{
         '100' : '100%',
         '70' : '400px'
       },
       gap:{
-        '2' : '2%'
+        '2' : '2%',
+        '15' : '20%',
       },
       fontFamily: {
         playfair: ["Playfair Display"],
