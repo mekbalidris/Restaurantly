dd418369ba47a7d4482b3adb088a3ee79e299775
mekbalidris <mekbalidriss@gmail.com>
2025-02-19 18:45:42 +0100

    fixing bugs

--- src/pages/Nav.jsx
+++ src/pages/Nav.jsx
@@ -91,8 +91,7 @@ export default function Nav({ setIsAuthenticated, isAuthenticated }) {
   const handleLogout = () => {
     localStorage.removeItem("accessToken");
     setIsAuthenticated(false);
-    navigate("/accessaccount");
-    window.location.reload();
+    navigate("/accessaccount", { replace: true});
   };
 
   return (
