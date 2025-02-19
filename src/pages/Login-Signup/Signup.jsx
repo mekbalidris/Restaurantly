1ffed4f0eb915c4fc5f8f016410c3c779fe1bf00
mekbalidris <mekbalidriss@gmail.com>
2025-02-19 19:01:45 +0100

    last commit

--- src/pages/Login-Signup/Signup.jsx
+++ src/pages/Login-Signup/Signup.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import process from '../../../.env'
 
 export default function SignUp({ setIsAuthenticated, isAuthenticated }) {
     const [formData, setFormData] = useState({
